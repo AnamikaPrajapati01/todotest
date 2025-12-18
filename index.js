48bd4c2a892e99e5bd2d16717e05be3687b4ed6f
AnamikaPrajapati <Subiprajapati5@gmail.com>
2025-12-18 13:20:18 +0545

    added css

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+let Tasks= JSON.parse(localStorage.getItem("tasks")) || []
 
-
-let task = []
 function getDataFromHtml(event) {
     event.preventDefault()
-    let task = event.target.task.value
-    task.push(task)
-    displayTasks()
+let task=event.target.task.value
+Tasks.push(task)
+localStorage.setItem("tasks",JSON.stringify(Tasks))
+displayTasks()
 }
 
+
 function displayTasks() {
-    let ul = document.getElementById("task-list")
-    let string = ""
-    task.forEach((el) => {
-        string += `<li>${el}</li>`
-    })
-    ul.innerHTML = string
+let ul=document.getElementById("task-list")
+let string=""
+Tasks.forEach((el,index)=>{
+string+=`<li>${el} <button onClick="deteleTask(${index})"><span class="material-symbols-outlined">
+delete
+</span></button></li>`
+})
+ul.innerHTML=string
 }
+
+
+displayTasks()
+
+
+function deteleTask(index) {
+Tasks.splice(index,1)
+localStorage.setItem("tasks",JSON.stringify(Tasks))
+displayTasks()
+}
+
